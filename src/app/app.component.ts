6f0b87b0d38526f3b925ce6b588ae09b7df9ba10
Ruben Lomo Maestu <ruben.lomomaestu@gmail.com>
2020-11-03 16:00:53 +0100

    data complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis sem vel libero condimentum faucibus sed sit amet ipsum. ',
+      loveIts: 0,
+      created_at: Date
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis sem vel libero condimentum faucibus sed sit amet ipsum. ',
+      loveIts: 1,
+      created_at: Date
+    },
+    {
+      title: 'Encore un post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis sem vel libero condimentum faucibus sed sit amet ipsum. ',
+      loveIts: 3,
+      created_at: Date
+    }
+
+  ];
+
+
+
 }
